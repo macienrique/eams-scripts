baffc58b5becddc9609f4c7eb862581347de661e
Enrique Macias <enrique.macias@scotiabank.com.pe>
2021-06-05 19:03:04 -0500

    feat: add check script

---
+++ src/commands/check/check.ts
@@ -0,0 +1,12 @@
+import { Environments } from '../../domain/script-arguments';
+import build from '../build/build';
+import lint from '../lint/lint';
+import test from '../test/test';
+
+const check = () => {
+  lint('-c');
+  test(['--coverage']);
+  build(Environments.Local);
+};
+
+export default check;
