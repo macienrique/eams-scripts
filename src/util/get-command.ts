15ec5ef8eb52681637f2f733b35afcf5c26e987f
Enrique Macias <enrique.macias@scotiabank.com.pe>
2021-06-23 13:47:07 -0500

    fix: add path.resolve to run eams-scripts in windows

--- src/util/get-command.ts
+++ src/util/get-command.ts
@@ -1,3 +1,5 @@
-const getCommandBinPath = (command: string) => `./node_modules/.bin/${command}`;
+import path from 'path';
+
+const getCommandBinPath = (command: string) => path.resolve(process.cwd(), 'node_modules', '.bin', command);
 
 export default getCommandBinPath;
